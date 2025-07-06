10cb28e987a8f4d89ae13bf39d28fb701cc82b44
Brandt Davis <brandtd77@gmail.com>
2025-07-06 16:11:14 -0600

    Add basic http server for backend

--- server/main.go
+++ server/main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type activity struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+	Type string `json:"type"`
+	Date string `json:"date"`
+}
+
+var activities = []activity{
+	{ID: "1", Name: "Easy Run", Type: "Run", Date: "2025-06-12"},
+}
+
+func getTodos(context *gin.Context) {
+	context.IndentedJSON(http.StatusOK, activities)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/activities", getTodos)
+	router.Run("localhost:8080")
+}
